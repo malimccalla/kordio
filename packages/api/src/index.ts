f46ba3ec82faf6f82dd23025276a63b672064156
Mali McCalla <malimccalla@gmail.com>
2022-08-31 10:04:10 +0100

    switch apollo-server to apollo-server-express

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,33 +1,62 @@
 import 'dotenv/config';
 
+import http from 'http';
+
 import { PrismaClient } from '@prisma/client';
-import { ApolloServer } from 'apollo-server';
-import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import {
+  ApolloServerPluginDrainHttpServer,
+  ApolloServerPluginLandingPageLocalDefault,
+} from 'apollo-server-core';
+import { ApolloServer } from 'apollo-server-express';
+import cors from 'cors';
+import express from 'express';
 
 import schema from './schema';
 
 const port = process.env.PORT || 4000;
-const prisma = new PrismaClient();
 const frontEndHost = 'http://localhost:3000';
 
-const apolloServer = new ApolloServer({
-  schema,
-  context: { prisma },
-  csrfPrevention: true,
-  cache: 'bounded',
-  cors: {
-    origin: [frontEndHost],
-  },
-  /**
-   * What's up with this embed: true option?
-   * These are our recommended settings for using AS;
-   * they aren't the defaults in AS3 for backwards-compatibility reasons but
-   * will be the defaults in AS4. For production environments, use
-   * ApolloServerPluginLandingPageProductionDefault instead.
-   **/
-  plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
-});
+const startServer = async () => {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const prisma = new PrismaClient();
 
-apolloServer.listen({ port }).then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
+  const corsOptions = {
+    origin: [frontEndHost],
+  };
+
+  app.use(cors(corsOptions));
+
+  const apolloServer = new ApolloServer({
+    schema,
+    context: { prisma },
+    csrfPrevention: true,
+    cache: 'bounded',
+    /**
+     * What's up with this embed: true option?
+     * These are our recommended settings for using AS;
+     * they aren't the defaults in AS3 for backwards-compatibility reasons but
+     * will be the defaults in AS4. For production environments, use
+     * ApolloServerPluginLandingPageProductionDefault instead.
+     **/
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+    ],
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({
+    app,
+    path: '/',
+    cors: corsOptions,
+  });
+
+  await new Promise<void>((resolve) => httpServer.listen({ port }, resolve));
+  console.log(
+    `🚀 Server ready at http://localhost:4000${apolloServer.graphqlPath}`
+  );
+};
+
+startServer();
