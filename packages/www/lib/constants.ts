609dfc372ac57b85d85b71b7cc95a88b54f81dcd
Mali McCalla <malimccalla@gmail.com>
2022-09-05 13:26:28 +0100

    fix public env vars

--- packages/www/lib/constants.ts
+++ packages/www/lib/constants.ts
@@ -1,4 +1,4 @@
 export const isBrowser = typeof window !== 'undefined';
 
-// In development these are defined in next.config.js
+// In local development these are defined in a .env.local file
 export const apiEndpoint = process.env.NEXT_PUBLIC_API_ENDPOINT;
--- packages/www/next.config.js
+++ packages/www/next.config.js
@@ -2,12 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    domains: ['uploads-ssl.webflow.com'],
-  },
-  env: {
-    NEXT_PUBLIC_API_ENDPOINT: 'http://localhost:4000',
-  },
   compiler: {
     styledComponents: true,
   },
